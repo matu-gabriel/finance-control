fa10d3cd6a5db1922e03238c4f35f78bd2e24102
Matusalem Gabriel <122733420+matu-gabriel@users.noreply.github.com>
2024-08-04 22:43:47 -0300

    Fazendo validação com yup

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,26 +1,38 @@
 import UserSchema from "../models/UserSchema";
+import * as Yup from "yup";
 
 class UserController {
   async store(req, res) {
-    const { name, email, password_hash } = req.body;
+    const schema = Yup.object().shape({
+      name: Yup.string().required("Name is required").min(2),
+      email: Yup.string()
+        .required("Email is required")
+        .email("Invalid email format"),
+      password_hash: Yup.string().required("Password is required").min(6),
+    });
+
     try {
-      const userExist = await UserSchema.findOne({ email });
-      if (userExist) {
-        return res.status(400).json({ messege: "User already exist" });
-      }
-
-      const newUser = await UserSchema.create({
-        name,
-        email,
-        password_hash,
-      });
-
-      return res
-        .status(201)
-        .json({ messege: "User created successfully", user: newUser });
+      schema.validateSync(req.body, { abortEarly: false });
     } catch (err) {
-      res.status(500).json({ messege: "Error when creating user: ", err });
+      return res.status(400).json({ errors: err.errors });
     }
+
+    const { name, email, password_hash } = req.body;
+
+    const userExist = await UserSchema.findOne({ email });
+    if (userExist) {
+      return res.status(400).json({ messege: "User already exist" });
+    }
+
+    const newUser = await UserSchema.create({
+      name,
+      email,
+      password_hash,
+    });
+
+    return res
+      .status(201)
+      .json({ messege: "User created successfully", user: newUser });
   }
 }
 
