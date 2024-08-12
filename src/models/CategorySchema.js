694a9b5357d5b016a7e9c639876ad0d2abf4dbdb
Matusalem Gabriel <122733420+matu-gabriel@users.noreply.github.com>
2024-08-11 23:54:52 -0300

    Criando schema de categoria

---
+++ src/models/CategorySchema.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const CategorySchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Category = mongoose.model("Category", CategorySchema);
+
+export default Category;
