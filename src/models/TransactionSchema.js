c0057d52f749c3c78b2e3523eff9cab272ab13e4
Matusalem Gabriel <122733420+matu-gabriel@users.noreply.github.com>
2024-08-14 20:53:59 -0300

    Criando o model schema de transaction

---
+++ src/models/TransactionSchema.js
@@ -0,0 +1,39 @@
+import { type } from "express/lib/response";
+import mongoose from "mongoose";
+
+const TransactionSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["income", "expense"],
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Transaction = mongoose.model("Transaction", TransactionSchema);
+
+export default Transaction;
