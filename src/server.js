e2caf7a3aba8bebd2f1d665de91e4742f8bacc80
Matusalem Gabriel <122733420+matu-gabriel@users.noreply.github.com>
2024-11-02 22:06:18 -0300

    Mudando a porta

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 import app from "./app.js";
 import connectDB from "./database/index.js";
+const port = process.env.PORT || 3000;
 
 const startServer = async () => {
   await connectDB();
 
-  app.listen(3000, () => console.log("Server is running at port 3000"));
+  app.listen(3000, () => console.log(`Server is running at port ${port}`));
 };
 
 startServer();
